4738efc1bd3a86c584f1aacee362bc6019924cac
deanglane <dglaneto@gmail.com>
2023-02-17 16:57:40 -0500

    orders to array and almost clear list

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const openModal = document.querySelector(".openModal");
 const closeModal = document.querySelector(".close-button");
 const browse = document.querySelector(".browse");
 const openOrderPage = document.querySelector("#orderPage"); // open order page
+const fakeWindow = document.querySelector("#fakeWindow"); // open order page fake window
 const placeOrder = document.querySelector(".placeOrder"); // add to order button
 const closeOrderPage = document.querySelector(".closeOrderPage");
 // Variables for selection modal
@@ -13,11 +14,18 @@ const itemName = document.querySelector("#menuName");
 const itemDesc = document.querySelector("#menuDescription");
 const itemPrice = document.querySelector("#menuPrice");
 
+const subTotal = document.getElementById("subTotal");
+const subTotalTax = document.getElementById("subTotalTax");
+const total = document.getElementById("total");
 
+
+
+let trash = null;
 let mealName = "";
 let mealPrice = 0;
 const mealSum = [];
-// modal.showModal();
+const orders = [];
+
 
 // modal & side order page
 
@@ -31,46 +39,103 @@ browse.addEventListener("click", function () {
 })
 
 closeOrderPage.addEventListener("click", function () {
-  openOrderPage.style.display = "none";
+  openOrderPage.style.display = "none"; 
+  fakeWindow.style.display = "none";
 })
 
-const subTotal = document.getElementById("subTotal");
-const subTotalTax = document.getElementById("subTotalTax");
-const total = document.getElementById("total");
 // add meals to the order form
 // this might need to be a submit button not click
 placeOrder.addEventListener("click", function () {
   modal.close();
+  fakeWindow.style.display = "block";
   openOrderPage.style.display = "block";
   
+  
   const meal = mealName;
-  console.log(meal);
 
-  
-  
   // create a new list element
   const newLi = document.createElement('li');
   newLi.innerHTML = `
-  <i class="fa-solid fa-trash-can"></i>
+  <img class="trash" src="./assets/trash-can-solid.svg" alt="" />
   <p>${meal}</p>
   <p>$${mealPrice}</p>
   `;
   // append new list element to the DOM
   const ulTarget = document.getElementById('clientOrder');
   ulTarget.appendChild(newLi);
-  
-  
+
+  const trash = document.querySelectorAll(".trash");
+
   // create an array for storing the meals price
   mealSum.push(Number(mealPrice)); // adds stored items to an array for total
-  
+
   // calculates the subTotal and return it
   const mealSumTotal = mealSum.reduce((totalValue, currentAmount) => {
     return totalValue + currentAmount;
   });
   // takes the tallied total of the ordered meals and passes it as an argument to the function taxCalc
+  orderArray(mealName, mealPrice);
   taxCalc(mealSumTotal);
 
+  console.log(trash);
+  
+  
+  getTrash(trash); // passed to the bin parameter
+  
+  console.log(trash.length);
+  // getTrash();
+  
 });
+
+
+// function to target the trash can thats clicked, return the menu item index and remove it from the orders array
+const getTrash = (bin) => {
+  for ( let j = 0; j < bin.length; j++ ) {
+    bin[j].addEventListener('click',function (){
+      // returns the name of the meal next to the trash
+      mealTarget = this.nextElementSibling.textContent;
+
+      // remove the returned index from the array
+      returnMeal = orders.findIndex(meal => meal.mealName === this.nextElementSibling.textContent);
+
+      // removes targeted index from the orders array
+      orders.splice(returnMeal,1);
+    });
+  };
+};    
+    
+
+// function to add meal name and meal price to an array
+const orderArray = (name, price) => {
+  orders.push({"mealName": name, "mealPrice": Number(price)});
+  return orders;
+};
+
+  
+
+
+//   // create a new list element
+//   const newLi = document.createElement('li');
+//   newLi.innerHTML = `
+//   <i class="fa-solid fa-trash-can"></i>
+//   <p>${meal}</p>
+//   <p>$${mealPrice}</p>
+//   `;
+//   // append new list element to the DOM
+//   const ulTarget = document.getElementById('clientOrder');
+//   ulTarget.appendChild(newLi);
+  
+  
+//   // create an array for storing the meals price
+//   mealSum.push(Number(mealPrice)); // adds stored items to an array for total
+  
+//   // calculates the subTotal and return it
+//   const mealSumTotal = mealSum.reduce((totalValue, currentAmount) => {
+//     return totalValue + currentAmount;
+//   });
+//   // takes the tallied total of the ordered meals and passes it as an argument to the function taxCalc
+//   taxCalc(mealSumTotal);
+// });
 
 // function to calculate the tax and totals and add them to the DOM
 const taxCalc = (sub) => {
@@ -85,7 +150,8 @@ const taxCalc = (sub) => {
 };
 
 // delete items from the order window
-
+// when you click the trash can it targets that rows menu item
+// 
 
 
 // menuItem.forEach((meal)=>{
@@ -126,10 +192,8 @@ for (let i = 0; i < menuItem.length; i++) {
     // console.log(itemPrice.textContent);
     itemPrice.textContent = e.target.nextElementSibling.firstElementChild.textContent;
 
-    return mealName, mealPrice
+    return mealName, mealPrice;
   })
 };
-
-
 
 
