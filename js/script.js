4646df1a238429d62d90e95ca47eacc3919324b2
deanglane <dglaneto@gmail.com>
2023-02-26 19:20:20 -0500

    fixed removing items

--- js/script.js
+++ js/script.js
@@ -18,14 +18,14 @@ const orderSubTotal = document.getElementById("subTotal");
 const subTotalTax = document.getElementById("subTotalTax");
 const total = document.getElementById("total");
 
-let trash = null;
-let mealName = ""; // temporary variable to store what menu item was added to order
-let mealPrice = 0; // THIS ONE
+let mealName = ""; 
+let mealPrice = 0;
 let mealValue = "";
-// const mealSum = []; // THIS ONE change to subTotalArr
+
 const orders = [];
 
-const subTotalArr = []; // array for storing the all the costs
+// const subTotalArr = []; // array for storing the all the costs
+let subTotalArr = []; // array for storing the all the costs
 let orderTotal = 0; // order total after tax has been applied to the subTotal 
 let subTotal = 0; // variable to store the summed total of all orders
 const tax = 0.13; // Tax to be applied to subTotal
@@ -57,69 +57,77 @@ placeOrder.addEventListener("click", function () {
   const meal = mealName;
   // create a new list element
   const newLi = document.createElement('li');
+  // newLi.innerHTML = `
+  // <img class="trash" id="${mealValue}" src="./assets/trash-can-solid.svg" alt="" />
+  // <p>${meal}</p>
+  // <p>$${mealPrice}</p>
+  // `;
   newLi.innerHTML = `
-  <img class="trash" src="./assets/trash-can-solid.svg" alt="" />
+  <button class="trash" id="${mealValue}"></button>
   <p>${meal}</p>
   <p>$${mealPrice}</p>
   `;
   // append new list element to the DOM
   const ulTarget = document.getElementById('clientOrder');
   ulTarget.appendChild(newLi);
-  const trash = document.querySelectorAll(".trash");
-  // console.log("what does trash return?", trash); // returns the nodeList item of img of the trashcan
+  // this creates the variable Trash and stores the targets of the trash can delete button
+  // console.log("*EventListener - whats in Trash?", trash);
   
-  // create an array for storing the meals price
-  // mealSum.push(Number(mealPrice)); // adds stored items to an array for total
-  subTotalArr.push(Number(mealPrice)); // adds stored items to an array for total
   // add value to new list item
   newLi.setAttribute("id",mealValue);
-  // takes the tallied total of the ordered meals and passes it as an argument to the function taxCalc
-  orderArray(mealName, mealPrice, mealValue);
-  getTrash(trash); // passed to the bin parameter
-  tallyTotal(); // call talleyTotal to tally and display totals
-  console.log("orders array after adding items",orders);
+  // console.log("what does trash return?", trash); // returns the nodeList item of img of the trashcan
   
+  // mealSum.push(Number(mealPrice)); // adds stored items to an array for total
+  // subTotalArr.push(Number(mealPrice)); // adds stored items to an array for total
+  orderArray(mealName, mealPrice, mealValue);
+  mapCostArr();
+  getSubTotal();
+  // takes the tallied total of the ordered meals and passes it as an argument to the function taxCalc
+  tallyTotal(); // call talleyTotal to tally and display totals
+
+  remove();
+
 });
+
 
 // REMOVING ITEMS FROM THE ORDER //
 // function to target the trash can thats clicked, return the menu item index and remove it from the orders array
-// TODO might need to use this event listener function to only only return the index and meal item name
-const getTrash = (bin) => {
-  for ( let j = 0; j < bin.length; j++ ) {
-    bin[j].addEventListener('click',function (){
-      // returns the name of the meal next to the trash
-      mealTarget = this.nextElementSibling.textContent;
-      console.log("the name of the item being removed = ",mealTarget);
-      // removing the li element of the targeted menu item
-      console.log("element being removed = ",this.parentElement);
-      this.parentElement.remove('li');
-      console.log("**element has been removed");
-      console.log("order array after target item was removed", orders); // returns empty
-      
-      // remove the returned index from the array
-      // TODO this needs adjusting - it is removing other items from the array except only the one I want
-      mealCostIndex = orders.findIndex(meal => meal.mealName === this.nextElementSibling.textContent);
-      console.log("index of selected array item", mealCostIndex);
-      // let returnCost = orders[mealCostIndex].mealPrice;
+// TODO - look into why this for each() method keeps looping
 
-      // removes targeted index from the orders array object
-      // orders.shift(mealCostIndex);
-      orders.splice(mealCostIndex,1);
-
-      //removes the meal from the order window
-
-      // TODO need to add the function to remove the selected items cost from totals and array?
-      // removed price from array
-      // subTotalArr.shift(mealCostIndex);
-      // console.log(subTotalArr);
-      
-      // subtractTotal();
+const remove = () => {
+  const bins = document.querySelectorAll(".trash");
+  console.log(bins);
+  
+  bins.forEach((bin) => {
+    bin.addEventListener('click', function(e){
+      // e.preventDefault();
+      const selectedItem = e.target.nextElementSibling.textContent;
+      const mealCostIndex = orders.findIndex(meal => meal.mealName === selectedItem);
+      e.target.parentElement.remove('li');
+      reCalc(mealCostIndex);
     });
-  };
-};    
-    
+  });
+};
 
-// function to add meal name, value and meal price to an array
+
+
+const reCalc = (index) => {
+  if(index < 0) {
+    console.log("error, item not found in array");
+  } else {
+    console.log("*reCalc-index of item to remove", index);
+    orders.splice(index,1);
+    console.log("*reCalc-item has been removed", orders);
+    // re-run mapping function and roll up costs
+    mapCostArr();
+    console.log("*reCalc-New Cost map Array", subTotalArr);
+    subtractTotal();
+    console.log("*reCalc-sub total", subTotal);
+  };
+};
+
+
+// function to add meal name, value and meal price to the orders array
 const orderArray = (name, price, value) => {
   orders.push({"mealName": name, "mealPrice": Number(price), "mealValue": value});
   return orders;
@@ -129,47 +137,56 @@ const orderArray = (name, price, value) => {
 // function to tally up all the values in the array    
 const getSubTotal = () => {
   // if conditional to check if the array is empty to avoid returning an error
-  if (subTotalArr.length === 0) {
+  if (subTotalArr.length <= 0) {
     // if the array is empty...
-    console.log("array is empty");
     subTotal = 0;
     orderTotal = 0;
   } else {
     // if the array has values...
+    // store the summed sub total amount in the subTotal Variable
     subTotal = subTotalArr.reduce((totalValue, currentAmount) => {
+      // return the array summed sub total amount
       return totalValue + currentAmount;
     });
-  }
-}
+  };
+};
+
+const mapCostArr = () => {
+  subTotalArr = orders.map((order)=>{
+    return order.mealPrice
+  });
+};
 
 // ? Could these two function be cleaner as they basically do the same thing
 // Tallying Totals for new additions
 const tallyTotal = () => {
-  // console.log("Adding Cost");
-  // console.log("new sub total array =", subTotalArr);
+  // console.log("*tallyTotal-EXECUTED...");
+  // console.log("*tallyTotal-RUNNING...");
   // calling the getSubTotal function to tally the array of values after the new cost has been added
   getSubTotal();
-  // console.log("Total after adding = ", subTotal);
   // storing the new grand total of the bill in a variable after tax has been applied
   orderTotal = (subTotal * tax) + subTotal;
-  // console.log("Total of entire order", orderTotal);
+  // console.log("*tallyTotal-total order amount", orderTotal);
+  
   // append new totals to the DOM
+  // console.log("*tallyTotal-appending DOM");
   orderSubTotal.textContent = (subTotal).toFixed(2);
   subTotalTax.textContent = (subTotal * tax).toFixed(2);
   total.textContent = (orderTotal).toFixed(2);
+  // console.log("*tallyTotal-COMPLETED");
 }
 
 // Subtracting Totals for new Subtractions
 const subtractTotal = () => {
-  // console.log("new sub total array =", subTotalArr);
+
   getSubTotal();
-  // console.log("Total after subtracting = ", subTotal);
   orderTotal = (subTotal * tax) + subTotal;
-  // console.log("Total of entire order", orderTotal);
+
   // append new totals to the DOM
   orderSubTotal.textContent = (subTotal).toFixed(2);
   subTotalTax.textContent = (subTotal * tax).toFixed(2);
   total.textContent = (orderTotal).toFixed(2);
+
 }
     
 // QUICK VIEW FOR SELECTED MENU ITEM //
@@ -187,7 +204,7 @@ for (let i = 0; i < menuItem.length; i++) {
     // add description to modal
     itemDesc.textContent = e.target.nextElementSibling.firstChild.textContent;
     itemPrice.textContent = e.target.nextElementSibling.firstElementChild.textContent;
-    return mealName, mealPrice, mealValue;
+    // return mealName, mealPrice, mealValue;
   })
 };
 
