68fdd9c061c35325f213ced2d9279000f6273b5d
deanglane <dglaneto@gmail.com>
2023-02-28 16:59:03 -0500

    added images and cancel order button

--- js/script.js
+++ js/script.js
@@ -2,14 +2,16 @@ const modal = document.querySelector("#modal");
 const openModal = document.querySelector(".openModal");
 const closeModal = document.querySelector(".close-button");
 const browse = document.querySelector(".browse");
+
+
 const openOrderPage = document.querySelector("#orderPage"); // open order page
 const fakeWindow = document.querySelector("#fakeWindow"); // open order page fake window
-const placeOrder = document.querySelector(".placeOrder"); // add to order button
+const orderPlace = document.querySelector(".placeOrder"); // add to order button
 const closeOrderPage = document.querySelector(".closeOrderPage");
 // Variables for selection modal
 const menuItem = document.querySelectorAll("li p:first-child"); // returns all menuItemHeadings into an NodeList Array
 const menuDesc = document.querySelectorAll("li p:nth-child(2)");
-const itemImg = document.getElementById("dean");
+const itemImg = document.getElementById("mealImage");
 const itemName = document.querySelector("#menuName");
 const itemDesc = document.querySelector("#menuDescription");
 const itemPrice = document.querySelector("#menuPrice");
@@ -20,7 +22,7 @@ const total = document.getElementById("total");
 
 let mealName = ""; 
 let mealPrice = 0;
-let mealValue = "";
+let mealId = "";
 
 const orders = [];
 
@@ -29,8 +31,6 @@ let subTotalArr = []; // array for storing the all the costs
 let orderTotal = 0; // order total after tax has been applied to the subTotal 
 let subTotal = 0; // variable to store the summed total of all orders
 const tax = 0.13; // Tax to be applied to subTotal
-
-
 
 
 // modal & side order page
@@ -48,9 +48,12 @@ closeOrderPage.addEventListener("click", function () {
   fakeWindow.style.display = "none";
 })
 
+
+
+
 // ADDING ITEMS TO THE ORDER FROM THE QUICK DISPLAY MODAL //
 // ?? This could also be a submit button? change click to submit? why?
-placeOrder.addEventListener("click", function () {
+orderPlace.addEventListener("click", function () {
   modal.close();
   fakeWindow.style.display = "block";
   openOrderPage.style.display = "block";
@@ -63,73 +66,106 @@ placeOrder.addEventListener("click", function () {
   // <p>$${mealPrice}</p>
   // `;
   newLi.innerHTML = `
-  <button class="trash" id="${mealValue}"></button>
+  <button class="trash" id="${mealId}"></button>
   <p>${meal}</p>
   <p>$${mealPrice}</p>
   `;
   // append new list element to the DOM
   const ulTarget = document.getElementById('clientOrder');
   ulTarget.appendChild(newLi);
+  remove();
   // this creates the variable Trash and stores the targets of the trash can delete button
   // console.log("*EventListener - whats in Trash?", trash);
   
   // add value to new list item
-  newLi.setAttribute("id",mealValue);
+  newLi.setAttribute("id", mealId);
+  newLi.setAttribute("id", "list");
   // console.log("what does trash return?", trash); // returns the nodeList item of img of the trashcan
   
   // mealSum.push(Number(mealPrice)); // adds stored items to an array for total
   // subTotalArr.push(Number(mealPrice)); // adds stored items to an array for total
-  orderArray(mealName, mealPrice, mealValue);
+  orderArray(mealName, mealPrice, mealId);
   mapCostArr();
   getSubTotal();
   // takes the tallied total of the ordered meals and passes it as an argument to the function taxCalc
   tallyTotal(); // call talleyTotal to tally and display totals
+  placeOrder();
+  cancel();
 
-  remove();
-
+  console.log(orders);
+  
 });
 
 
 // REMOVING ITEMS FROM THE ORDER //
 // function to target the trash can thats clicked, return the menu item index and remove it from the orders array
-// TODO - look into why this for each() method keeps looping
 
 const remove = () => {
   const bins = document.querySelectorAll(".trash");
-  console.log(bins);
   
   bins.forEach((bin) => {
     bin.addEventListener('click', function(e){
-      // e.preventDefault();
       const selectedItem = e.target.nextElementSibling.textContent;
       const mealCostIndex = orders.findIndex(meal => meal.mealName === selectedItem);
       e.target.parentElement.remove('li');
       reCalc(mealCostIndex);
+      console.log(mealCostIndex);
+      
     });
   });
 };
 
 
+const placeOrder = () => {
+  const orderPlacedModal = document.getElementById
+  ('modalOrderPlaced');
+  const placeOrderBtn = document.getElementById('placeOrder');
+  const closeThanks = document.getElementById('closeThanks');
+  
+  placeOrderBtn.addEventListener('click', function() {
+    openOrderPage.style.display = "none";
+    fakeWindow.style.display = "none";
+    orderPlacedModal.showModal();
+    
+  });
+  closeThanks.addEventListener("click", function () {
+    orderPlacedModal.close();
+  });
+};
+
+
+
+// function to cancel all orders and clear the totals
+const cancel = () => {
+  const cancel = document.getElementById("cancel");
+  const allLi = document.querySelectorAll('#list')
+
+  allLi.forEach((li) => {
+    cancel.addEventListener('click', function () {
+      li.remove('#list');
+      orders.splice(0, orders.length);
+      mapCostArr();
+      subtractTotal();
+      openOrderPage.style.display = "none";
+      fakeWindow.style.display = "none";
+    });
+  });
+};
+
 
 const reCalc = (index) => {
-  if(index < 0) {
-    console.log("error, item not found in array");
-  } else {
-    console.log("*reCalc-index of item to remove", index);
-    orders.splice(index,1);
-    console.log("*reCalc-item has been removed", orders);
+  if (index >= 0) {
+    orders.splice(index, 1);
     // re-run mapping function and roll up costs
     mapCostArr();
-    console.log("*reCalc-New Cost map Array", subTotalArr);
     subtractTotal();
-    console.log("*reCalc-sub total", subTotal);
   };
 };
 
 
 // function to add meal name, value and meal price to the orders array
-const orderArray = (name, price, value) => {
-  orders.push({"mealName": name, "mealPrice": Number(price), "mealValue": value});
+const orderArray = (name, price, id) => {
+  orders.push({"mealName": name, "mealPrice": Number(price), "mealValue": id});
   return orders;
 };
 
@@ -160,20 +196,15 @@ const mapCostArr = () => {
 // ? Could these two function be cleaner as they basically do the same thing
 // Tallying Totals for new additions
 const tallyTotal = () => {
-  // console.log("*tallyTotal-EXECUTED...");
-  // console.log("*tallyTotal-RUNNING...");
   // calling the getSubTotal function to tally the array of values after the new cost has been added
   getSubTotal();
   // storing the new grand total of the bill in a variable after tax has been applied
   orderTotal = (subTotal * tax) + subTotal;
-  // console.log("*tallyTotal-total order amount", orderTotal);
   
   // append new totals to the DOM
-  // console.log("*tallyTotal-appending DOM");
   orderSubTotal.textContent = (subTotal).toFixed(2);
   subTotalTax.textContent = (subTotal * tax).toFixed(2);
   total.textContent = (orderTotal).toFixed(2);
-  // console.log("*tallyTotal-COMPLETED");
 }
 
 // Subtracting Totals for new Subtractions
@@ -197,7 +228,8 @@ for (let i = 0; i < menuItem.length; i++) {
     modal.showModal();
     mealName = e.target.textContent;
     mealPrice = e.target.nextElementSibling.firstElementChild.textContent;
-    mealValue = e.target.attributes.value.textContent
+    mealId = e.target.attributes.id.textContent
+   
     itemImg.attributes.src.textContent = e.target.attributes.value.textContent;
     // add selection to the header inside modal
     itemName.textContent = e.target.textContent;
